b484c9620e464a8dedbe6025d1e02fbeb31a89be
miyazakiyamato <146906497+miyazakiyamato@users.noreply.github.com>
2024-05-01 15:17:55 +0900

    02_09_ex1

--- DirectXGame/EnemyBullet.cpp
+++ DirectXGame/EnemyBullet.cpp
@@ -2,12 +2,17 @@
 #include "TextureManager.h"
 #include <cassert>
 #include "MyMtVector3.h"
+#include "MyMtMatrix.h"
 
 void EnemyBullet::Initialize(Model* model, const Vector3& position, const Vector3& velocity) {
 	assert(model);
 	model_ = model;
 	worldTransform_.Initialize();
 	worldTransform_.translation_ = position;
+	worldTransform_.scale_ = {0.5f, 0.5f, 3.0f};
+	worldTransform_.rotation_.y = std::atan2f(velocity.x,velocity.z);
+	Vector3 velocityZ = MyMtMatrix::Transform(velocity, MyMtMatrix::MakeRotateYMatrix(-worldTransform_.rotation_.y));
+	worldTransform_.rotation_.x = std::atan2f(-velocityZ.y, velocityZ.z);
 	worldTransform_.UpdateMatrix();
 	velocity_ = velocity;
 	textureHandle_ = TextureManager::Load("./Resources/red.png");
